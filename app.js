d7d1fae993688a9eefb000e96e22832d6f2f7371
JD Marty <joshua.dmarty@gmail.com>
2020-12-09 20:52:59 -0800

    created main menu prompts

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+//npm packages
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+//connection module
 const connection = require("./config/connection");
+
+//prompts
+const { mainMenu } = require("./prompts")
 
 connection.connect((err) => {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     console.log("Welcome to Node Employee Tracking");
+
     connection.end();
-})
+});
+
---
+++ prompts/index.js
@@ -0,0 +1,24 @@
+const mainMenu = [
+  {
+    name: "mainSelection",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View Employees by Department",
+      "View Employees by Role",
+      "View Employees by Manager",
+      "Add an Employee",
+      "Add a Role",
+      "Add a Department",
+      "Remove an Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+      "View Utilized Budget by Department"
+    ],
+  },
+];
+
+module.exports = {
+  mainMenu,
+};
