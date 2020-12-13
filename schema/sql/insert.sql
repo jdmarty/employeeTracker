1bb3ad07bd01f00aba8a3e3d01bf3787f77031b0
JD Marty <joshua.dmarty@gmail.com>
2020-12-12 19:31:00 -0800

    added sql files in sql folder for reference

---
+++ schema/sql/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee SET ?;
+
+INSERT INTO role SET ?;
+
+INSERT INTO department SET ?;
--- schema/sql/select.sql
+++ schema/sql/select.sql
@@ -1,9 +1,11 @@
+-- Select all employees
 SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, CONCAT(m.first_name,' ',m.last_name) AS Manager, role.salary AS Salary
 FROM employee e
 INNER JOIN role ON e.role_id = role.id
 INNER JOIN department ON role.department_id = department.id
 LEFT JOIN employee m ON e.manager_id = m.id
 
+-- Select by department
 SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, CONCAT(m.first_name,' ',m.last_name) AS Manager, role.salary AS Salary
 FROM employee e
 INNER JOIN role ON e.role_id = role.id
@@ -11,6 +13,7 @@ INNER JOIN department ON role.department_id = department.id
 LEFT JOIN employee m ON e.manager_id = m.id
 WHERE department.name = 'Admin'
 
+-- Select by employee name
 SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, CONCAT(m.first_name,' ',m.last_name) AS Manager, role.salary AS Salary
 FROM employee e
 INNER JOIN role ON e.role_id = role.id
@@ -18,9 +21,17 @@ INNER JOIN department ON role.department_id = department.id
 LEFT JOIN employee m ON e.manager_id = m.id
 WHERE CONCAT(m.first_name,' ',m.last_name) = 'Drew Pullman'
 
+-- Select by role
 SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, CONCAT(m.first_name,' ',m.last_name) AS Manager, role.salary AS Salary
 FROM employee e
 INNER JOIN role ON e.role_id = role.id
 INNER JOIN department ON role.department_id = department.id
 LEFT JOIN employee m ON e.manager_id = m.id
 WHERE role.title = 'Line Leader'
+
+-- Select sum of salaries by department
+SELECT SUM(role.salary)
+FROM employee
+INNER JOIN role ON employee.role_id = role.id
+INNER JOIN department ON role.department_id = department.id
+WHERE department.name = "Production";
